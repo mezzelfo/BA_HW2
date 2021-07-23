9a2f50d7c4894b9c82c739e6111ee780a3805e25
mezzelfo <luca.mezzelfo@gmail.com>
2021-07-23 15:14:44 +0200

    timebucket corto e intero

--- main.py
+++ main.py
@@ -14,60 +14,21 @@ problem = Problem.from_multiple(
     make=4*4
 )
 
-# problem = Problem.from_multiple(
-#     demand_probs=[
-#         [1,2,3,4,5,6],
-#         [6,5,4,3,2,1],
-#     ],
-#     max_inv=40,
-#     make=12
-# )
-
-
-
 V, Q = problem.value_iteration(gamma=0.99)
 mu = Q.argmin(0)
 
-np.save('mu_opt.npy',mu.cpu().numpy())
-exit()
+#np.save('mu_opt.npy',mu.cpu().numpy())
+#exit()
 
-# import matplotlib.pyplot as plt
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection = '3d')
-
-# plt.matshow(mu[0,:,:,2],vmin=0,vmax=3)
-# plt.colorbar()
-# plt.matshow(mu[0,:,:,18],vmin=0,vmax=3)
-# plt.colorbar()
-
-# inv = torch.arange(0,20+1)
-# X, Y, Z = torch.meshgrid(inv,inv,inv)
-# list_of_pts = torch.vstack([X.ravel(), Y.ravel(), Z.ravel()]).T
-# mulist = mu[0].ravel()
-# list_of_pts = list_of_pts[mulist > 0]
-# mulist = mulist[mulist > 0]
-# print(list_of_pts.shape)
-# ax.contour(X,Y,Z, c = mu)
-# plt.show()
-
-# np.savetxt('V.csv',V.cpu().numpy().ravel(),delimiter = ',')
-# np.savetxt('Q.csv',Q.cpu().numpy().ravel(),delimiter = ',')
-# np.savetxt('mu.csv',mu.cpu().numpy().ravel(),delimiter = ',')
-
-# np.save('mu.npy',mu.cpu().numpy())
-# np.save('Q.npy',Q.cpu().numpy())
-# np.save('V.npy',V.cpu().numpy())
-
-exit()
-
-init_inventory = [1,1,1,1]
+init_time_bucket = 0
 init_machine = 0
-vtrue = V[tuple([init_machine])+tuple(init_inventory)].item()
+init_inventory = [20,20,20,20]
+vtrue = V[tuple([init_time_bucket,init_machine])+tuple(init_inventory)].item()
 print(vtrue)
 
 res = problem.simulate(
-    init_inventory, init_machine, mu.cpu(),
-    time_steps = 500, repetitions = 1000, gamma = 0.99
+    init_inventory, init_machine, init_time_bucket, mu.cpu(),
+    time_steps = 1000, repetitions = 1000, gamma = 0.99
 )
 print(res.mean(), res.min(), res.max(), res.shape)
 
--- withstatus.py
+++ withstatus.py
@@ -90,41 +90,58 @@ class Problem():
         self.num_actions = self.num_item+1
         self.inv_dimensions = [d+1 for d in self.states.max_invs]
 
-    def dynamic(self, inventory_state, machine_state, action, demand):
+    def dynamic(self, remaining_time_buckets, inventory_state, machine_state, action, demand):
+        # remaining_time_buckets: 0,1,2
+        # inventory_state: quattro scalari tra 0 e 20
+        # machine_state: 0,1,2,3 (indica l'era del prodotto)
+        # action: 0,1,2,3,4 (0 indica idle)
         cost = 0.0
+        item_to_prod = action-1
+
+        #IDLE
+        next_machine_state = machine_state
+        next_remaining_time_buckets = remaining_time_buckets
+        next_inv_state = np.copy(inventory_state)
+        
         if action > 0:
-            item_to_prod = action-1
-            if action == machine_state:
-                inventory_state[item_to_prod] += self.makes[item_to_prod]
-            elif action % 2 == machine_state % 2:
-                inventory_state[item_to_prod] += self.makes[item_to_prod]//2
-                cost += 40
+            next_machine_state = item_to_prod
+            next_remaining_time_buckets = max(remaining_time_buckets-1,0)
+            if item_to_prod != machine_state: #CAMBIO IDEA
+                if item_to_prod % 2 == machine_state % 2:
+                    next_remaining_time_buckets = 1
+                    cost += 40
+                else:
+                    next_remaining_time_buckets = 2
+                    cost += 80
             else:
-                inventory_state[item_to_prod] += self.makes[item_to_prod]//4
-                cost += 80
-        tmp_state = inventory_state-demand
+                if remaining_time_buckets == 0:
+                    next_inv_state[item_to_prod] += self.makes[item_to_prod]
+                else:
+                    cost += 10
+
+        tmp_state = next_inv_state-demand
         unsatisfied_demand = -tmp_state[tmp_state < 0]
         cost += 100*unsatisfied_demand.sum().item()
         tmp_state[tmp_state < 0] = 0
         # TODO: maybe switch order next two lines
         tmp_state = np.minimum(tmp_state, self.states.max_invs)
         cost += tmp_state.sum().item()
-        return cost, tmp_state, action
+        return cost, tmp_state, next_machine_state, next_remaining_time_buckets
 
-    def single_dynamic(self, item_to_prod, initial_inventory_state, action, demand):
+    def single_dynamic(self, item_single, initial_inventory_state, flag, demand):
         inv_state = initial_inventory_state
         cost = 0
 
-        if action == 0: # 0 - Do not produce
+        if flag == 0: # 0 - Do not produce
             pass
-        elif action == 1:  # 1 - startup from different familiy
-            inv_state += self.makes[item_to_prod]//4
+        elif flag == 1: # 1 - to different familiy
             cost += 80
-        elif action == 2: # 2 - startup from same familiy
-            inv_state += self.makes[item_to_prod]//2
+        elif flag == 2: # 2 - to same familiy
             cost += 40
-        elif action == 3: # 3 - no startup
-            inv_state += self.makes[item_to_prod]
+        elif flag == 3: # 3 - continue setup
+            cost += 10
+        elif flag == 4: # 4 - produce
+            inv_state += self.makes[item_single]
         else:
             raise NotImplementedError()
 
@@ -132,23 +149,21 @@ class Problem():
         unsatisfied_demand = -min(inv_tmp_state, 0)
         cost += 100*unsatisfied_demand
         inv_tmp_state = min(max(inv_tmp_state, 0),
-                            self.states.max_invs[item_to_prod])
+                            self.states.max_invs[item_single])
         cost += inv_tmp_state
         return cost, inv_tmp_state
 
     def generate_Pi_and_R_by_decomposition(self):
         matrices = []
         rewards = []
+        SINGLE_ACTIONS = [0,1,2,3,4]
+
         for item in range(self.num_item):
-            matrix = np.zeros([4]+[self.inv_dimensions[item]]*2)
-            reward = np.zeros([4]+[self.inv_dimensions[item]])
+            matrix = np.zeros([len(SINGLE_ACTIONS)]+[self.inv_dimensions[item]]*2)
+            reward = np.zeros([len(SINGLE_ACTIONS)]+[self.inv_dimensions[item]])
             for initial_inventory_state in self.states.single_iter(item):
                 for (demand, prob) in self.demands.single_iter(item):
-                    # 0 - Do not produce
-                    # 1 - startup from different familiy
-                    # 2 - startup from same familiy
-                    # 3 - no startup
-                    for action in [0, 1, 2, 3]:
+                    for action in SINGLE_ACTIONS:
                         cost, next_state = self.single_dynamic(
                             item, initial_inventory_state, action, demand
                         )
@@ -160,19 +175,30 @@ class Problem():
 
         return matrices, rewards
 
-    def get_single_action(self, global_action, machine_state, item_idx):
-        if global_action != item_idx+1:                 #non sto per produrre item_idx
+    def get_single_action(self, global_action, machine_state, item_single, remaining_time_buckets):
+        # machine_state: 0,1,2,3 (indica l'era del prodotto)
+        # global_action: 0,1,2,3,4 (0 indica idle)
+        # item_single: 0,1,2,3 (indica il singolo prodotto che sta interrogando la funzione)
+        
+        if global_action != item_single+1: #non sto per produrre item_single
             return 0
-        elif global_action == machine_state:            #sto continuando a produrre item_idx
-            return 3
-        elif global_action % 2 == machine_state % 2:    #prodotto precedente della stessa famiglia
-            return 2
-        else:                                           #prodotto precedente di famiglia diversa
-            return 1
 
+        # DA QUI IN POI global_action == item_single+1 e global_action > 0
+        # DA QUI IN POI mi dimentico di global_action, vogliamo produrre item_single
+        if item_single != machine_state: #ABBIAMO CAMBIATO IDEA
+            if item_single % 2 != machine_state % 2:
+                return 1
+            else:
+                return 2
+        else:
+            if remaining_time_buckets > 0:
+                return 3
+            else:
+                return 4
+        
     def value_iteration(self, gamma=0.99, device='cuda'):
-        V = torch.zeros([self.num_actions]+self.inv_dimensions).to(device)
-        Q = torch.rand([self.num_actions]*2+self.inv_dimensions).to(device)
+        V = torch.zeros([3]+[self.num_item]+self.inv_dimensions).to(device)
+        Q = torch.rand([self.num_actions]+[3]+[self.num_item]+self.inv_dimensions).to(device)
         Ms, Rs = self.generate_Pi_and_R_by_decomposition()
         Ms = [torch.from_numpy(m).float().to(device) for m in Ms]
         Rs = [torch.from_numpy(r).float().to(device) for r in Rs]
@@ -180,26 +206,40 @@ class Problem():
         start = time.time()
         for iteration in range(200000):
             # print(iteration)
-            for machine_state in range(self.num_actions):
-                for action in range(self.num_actions):
-                    selectedMs = [Ms[i][self.get_single_action(action, machine_state, i)]
-                                      for i in range(self.num_item)]
-                    selectedRs = [Rs[i][self.get_single_action(action, machine_state, i)]
-                                      for i in range(self.num_item)]
+            for remaining_time_buckets in range(3):
+                for machine_state in range(self.num_item):
+                    for action in range(self.num_actions):
+                        selectedMs = [Ms[i][self.get_single_action(action, machine_state, i, remaining_time_buckets)]
+                                        for i in range(self.num_item)]
+                        selectedRs = [Rs[i][self.get_single_action(action, machine_state, i, remaining_time_buckets)]
+                                        for i in range(self.num_item)]
 
-                    #Vnew = gamma*np.einsum('ABCD,iA,jB,kC,lD->ijkl',V,*selectedMs,optimize='optimal')
-                    Vnew = gamma * \
-                        functools.reduce(lambda W, m: torch.tensordot(
-                            W, m, dims=([0], [1])), selectedMs, V[action])
+                        item_to_prod = action-1
+                        # IDLE
+                        next_machine_state = machine_state
+                        next_remaining_time_buckets = remaining_time_buckets
+                        
+                        if action > 0:
+                            next_machine_state = item_to_prod
+                            next_remaining_time_buckets = max(remaining_time_buckets-1,0)
+                            if item_to_prod != machine_state:
+                                if item_to_prod % 2 == machine_state % 2:
+                                    next_remaining_time_buckets = 1
+                                else:
+                                    next_remaining_time_buckets = 2
 
-                    for i in range(self.num_item):
-                        Vnew += selectedRs[i].reshape([1]
-                                                    * i+[-1]+[1]*(self.num_item-i-1))
+                        Vnew = gamma * \
+                            functools.reduce(lambda W, m: torch.tensordot(
+                                W, m, dims=([0], [1])), selectedMs, V[next_remaining_time_buckets, next_machine_state])
 
-                    Q[action, machine_state] = Vnew
+                        for i in range(self.num_item):
+                            Vnew += selectedRs[i].reshape([1]
+                                                        * i+[-1]+[1]*(self.num_item-i-1))
+
+                        Q[action, remaining_time_buckets, machine_state] = Vnew
             Vnew = Q.min(0)[0]
 
-            if (V-Vnew).abs().max().item() < 1e-15:  # torch.allclose(V, Vnew):
+            if (V-Vnew).abs().max().item() < 1e-10:  # torch.allclose(V, Vnew):
                 print(f'Converged at iteration {iteration}!')
                 break
             V = Vnew
@@ -253,17 +293,18 @@ class Problem():
         print(f'Elapsed time decoupled: {time.time()-start}')
         return V, Q
 
-    def simulate(self, initial_inventory_state, initial_machine_state, policy, time_steps = 500, repetitions = 100, gamma = 0.99):
+    def simulate(self, initial_inventory_state, initial_machine_state, initial_remaining_time_buckets, policy, time_steps = 500, repetitions = 100, gamma = 0.99):
         policy = policy.cpu().numpy()
         total_costs = np.zeros(repetitions)
         for r in range(repetitions):
             inventory_state = np.array(initial_inventory_state)
             machine_state = initial_machine_state
+            remaining_time_buckets = initial_remaining_time_buckets
             for t in range(time_steps):
-                action = policy[tuple([machine_state])+tuple(inventory_state)]
+                action = policy[tuple([remaining_time_buckets, machine_state])+tuple(inventory_state)]
                 demands = self.demands.sample(1).flatten().cpu().numpy()
-                cost, inventory_state, machine_state = self.dynamic(inventory_state, machine_state, action, demands)
-                machine_state = machine_state.item()
+                cost, inventory_state, machine_state, remaining_time_buckets = self.dynamic(remaining_time_buckets, inventory_state, machine_state, action, demands)
+                machine_state = machine_state
                 total_costs[r] += cost * (gamma**t)
         return total_costs
 
